f362a1cf7a39da07c8ad8f3e0ab7563a1d684d49
neilellis <hello@neilellis.me>
2015-07-30 00:42:48 +0100

    Fixing test script

--- test.sh
+++ test.sh
@@ -6,6 +6,11 @@ cd wrapper
 docker build -t wrapper .
 cd ../helloworld
 mvn clean install
+if [ -n "$CIRCLE_CI" ]
+then
+docker run -ti -v /run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ wrapper hello-docker hello-world
+else
 docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ wrapper hello-docker hello-world
+fi
 docker run -ti hello-docker
 
