edb8186d87b02fb3eddc8c6a7d888635df35b9f5
neilellis <hello@neilellis.me>
2015-07-30 00:57:48 +0100

    Fixing test script

--- test.sh
+++ test.sh
@@ -12,9 +12,9 @@ then
     #    disabled at the moment
     #sudo docker run -ti --privileged -v /run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ wrapper sillelien/cyborg-helloworld:${CIRCLE_BRANCH} hello-world
     echo "Skipping CircleCI"
+    #docker run -ti sillelien/cyborg-helloworld:${CIRCLE_BRANCH}
+    #docker push sillelien/cyborg-helloworld:${CIRCLE_BRANCH}
 else
     docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ wrapper sillelien/cyborg-helloworld:${CIRCLE_BRANCH} hello-world
 fi
-docker run -ti sillelien/cyborg-helloworld:${CIRCLE_BRANCH}
-docker push sillelien/cyborg-helloworld:${CIRCLE_BRANCH}
 
