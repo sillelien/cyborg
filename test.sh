ecb39ca3206f748d4aa45f3797f8d8bdb5aae1ce
neilellis <hello@neilellis.me>
2015-07-30 00:16:16 +0100

    Working off jess now

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -ex
 docker pull sillelien/base-robovm
 cd $(dirname $0)
 cd wrapper
 docker build -t wrapper .
 cd ../helloworld
-docker run -ti -v $(pwd)/:/build/ wrapper hello-docker hello-world
+docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ wrapper hello-docker hello-world
 docker run -ti hello-docker
 
