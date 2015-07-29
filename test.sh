b1059e8e2ce26185ac31f6381728f1646278abbe
neilellis <hello@neilellis.me>
2015-07-30 00:24:53 +0100

    Fixing test script

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ cd $(dirname $0)
 cd wrapper
 docker build -t wrapper .
 cd ../helloworld
+mvn clean install
 docker run -ti -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/:/build/ wrapper hello-docker hello-world
 docker run -ti hello-docker
 
